926072c03eb740c60180bdb0c3301ba0865d079b
Claudia Borghini <claudiaborghini@Claudias-MacBook-Air.local>
2020-07-15 19:01:28 +0200

    Initial changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
-# your code goes here
+require 'pry'
+class Person
+
+  def initialize(name, bank_account=25, happiness=8, hygiene=8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness_level
+    if @happiness > 10 
+        @happiness = 10
+    elsif @happiness < 0
+        @happiness = 0
+    end
+  end
+
+  def hygiene_level
+    if @hygiene > 10
+        @hygiene = 10
+    elsif @hygiene < 0
+        @hygiene = 0
+  end
+
+  def clean?
+
+
+
+
+
+end

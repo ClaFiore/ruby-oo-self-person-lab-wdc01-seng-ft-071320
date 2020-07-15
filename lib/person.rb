225a8c596f2c8422893464ae1c44521e70343826
temorymatine <temorymatine@gmail.com>
2020-07-15 14:40:52 -0400

    m

--- lib/person.rb
+++ lib/person.rb
@@ -1,32 +1,55 @@
-require 'pry'
+
 class Person
 
-  def initialize(name, bank_account=25, happiness=8, hygiene=8)
-    @name = name
-    @bank_account = bank_account
-    @happiness = happiness
-    @hygiene = hygiene
-  end
+attr_reader :name,  :happiness, :hygiene
+attr_accessor :bank_account
 
-  def happiness_level
-    if @happiness > 10 
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def happiness=(new_happiness)
+    if new_happiness > 10 
         @happiness = 10
-    elsif @happiness < 0
+    elsif new_happiness < 0
         @happiness = 0
+    else
+        @happiness = new_happiness
     end
   end
 
-  def hygiene_level
-    if @hygiene > 10
+  def hygiene=(new_hygiene)
+    if new_hygiene > 10
         @hygiene = 10
-    elsif @hygiene < 0
+    elsif new_hygiene < 0
         @hygiene = 0
+    else
+        @hygiene = new_hygiene
+    end
   end
 
   def clean?
+     @hygiene > 7
+  end
 
-
-
-
+  def happy?
+    @happiness > 7 
+  end
+  
+  def get_paid(salary_amount)
+    @bank_account += salary_amount 
+    "all about the benjamins"
+  end 
+  
+  def take_bath
+    @hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
 
 end
+
+john = Person.new("John")
+puts john.clean?
